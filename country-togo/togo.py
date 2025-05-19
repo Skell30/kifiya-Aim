f26f7f5e0bd7733c8664e6561ea5ba885b2c942d
Skell30 <myas30714@gmail.com>
2025-05-19 10:47:24 +0300

    header

---
+++ country-togo/togo.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+#import seaborn as sns
+#import matplotlib.pyplot as plt
+
+try:
+    togo=pd.read_csv("")
+    print("Data Imported")
+except:
+    print("Try again!")    
